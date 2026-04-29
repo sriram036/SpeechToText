513a18cfb0fee1a8fcd0ea94d5c066344abf8b3a
sriram036 <sriramravichandran2608@gmail.com>
2026-04-29 22:16:14 +0530

    Speech to Text Modified

--- SpeechToTextApp/src/App.jsx
+++ SpeechToTextApp/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import SpeechToText from "./Components/SpeechToText";
+import SpeechToText from "./components/SpeechToText";
 
 
 
